583ce851271f7d991580045b139063dac10cb600
Turkay Tunc <turkaytunc00@gmail.com>
2020-02-02 17:29:13 +0300

    Added data fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from "react";
 import "./App.css";
 import "./logo.svg";
+import axios from "axios";
 
-const App = () => {
-  return <div className="App">Hey</div>;
-};
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      recipes: []
+    };
+  }
+  componentDidMount() {
+    const key = "chicken";
+    const APP_ID = "caed25b5";
+    const APP_KEY = "09a9d503749688f8a251838da8089fca";
+    let query = `https://api.edamam.com/search?q=${key}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    axios.get(query).then(res => this.setState({ recipes: res.data.hits }));
+  }
+  render() {
+    let items = this.state.recipes
+      .map(recipe => recipe.recipe)
+      .map(item => {
+        return (
+          <div>
+            <div>{item.label}</div>
+            <img src={item.image}></img>
+          </div>
+        );
+      });
+    console.log(this.state.recipes.map(recipe => recipe.recipe));
+    return <div className="App">{items}</div>;
+  }
+}
 
 export default App;
