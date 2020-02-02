790ed5f93c8a2bbab32272e51057e2277b494909
Turkay Tunc <turkaytunc00@gmail.com>
2020-02-02 18:03:47 +0300

    Update style

--- src/App.js
+++ src/App.js
@@ -9,14 +9,16 @@ class App extends React.Component {
     this.state = {
       recipes: []
     };
+    // this.showRecipe = this.showRecipe.bind(this);
   }
   componentDidMount() {
-    const key = "chicken";
+    const key = "banana";
     const APP_ID = "caed25b5";
     const APP_KEY = "09a9d503749688f8a251838da8089fca";
     let query = `https://api.edamam.com/search?q=${key}&app_id=${APP_ID}&app_key=${APP_KEY}`;
     axios.get(query).then(res => this.setState({ recipes: res.data.hits }));
   }
+
   render() {
     let items = this.state.recipes
       .map(recipe => recipe.recipe)
