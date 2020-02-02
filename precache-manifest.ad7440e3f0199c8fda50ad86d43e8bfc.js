c4a0abb1743606d912450a40967243711c77d66e
Turkay Tunc <turkaytunc00@gmail.com>
2020-02-02 15:09:45 +0300

    Updates

---
+++ precache-manifest.ad7440e3f0199c8fda50ad86d43e8bfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32321283a046f5ca05ed15f1c3223106",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "9227c4cf93f493e8cb20",
+    "url": "/recipe-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1f33aaa192bbd90355df",
+    "url": "/recipe-app/static/js/2.b3611cfd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/recipe-app/static/js/2.b3611cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9227c4cf93f493e8cb20",
+    "url": "/recipe-app/static/js/main.88df22d1.chunk.js"
+  },
+  {
+    "revision": "148e400df5fd6abd3a3f",
+    "url": "/recipe-app/static/js/runtime-main.5fe0ecde.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recipe-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
