f83eaa61324336b618b094f0b39d1a78c3e9253a
Turkay Tunc <turkaytunc00@gmail.com>
2020-02-03 10:18:51 +0300

    Updates

--- precache-manifest.82b45567c83a8d00d6f063d30584b01e.js
+++ precache-manifest.82b45567c83a8d00d6f063d30584b01e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "772e7d01eb36a5511b05cf6a368df37f",
+    "revision": "62286089c1eb5371ca7d7e5374a27e5c",
     "url": "/recipe-app/index.html"
   },
   {
-    "revision": "d4fa311729211c6c08bd",
-    "url": "/recipe-app/static/css/main.67f00cae.chunk.css"
+    "revision": "90c0240b0381398d9d3c",
+    "url": "/recipe-app/static/css/main.4aa26b0a.chunk.css"
   },
   {
     "revision": "516f7a24e579ada39336",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/recipe-app/static/js/2.5673249a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d4fa311729211c6c08bd",
-    "url": "/recipe-app/static/js/main.52df269a.chunk.js"
+    "revision": "90c0240b0381398d9d3c",
+    "url": "/recipe-app/static/js/main.c7a8fd24.chunk.js"
   },
   {
     "revision": "148e400df5fd6abd3a3f",
